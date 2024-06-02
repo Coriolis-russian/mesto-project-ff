2f2596635de16b2afee47b5b8b615d006a8ba064
Anton Aksenov <Coriolis@inbox.ru>
2024-06-02 20:13:15 +0300

    +Работа с картами: добавление и удаление

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+// Шаблон карты карточки
+const cardTemplateHolder = document.querySelector('#card-template');
+console.assert(!!cardTemplateHolder, 'Не найден шаблон карт "card-template" в html файле');
+cardTemplate = cardTemplateHolder.content.querySelector('.card');
 
-// @todo: DOM узлы
+// DOM узлы
+const placesList = document.querySelector('.page .places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(name, imgLink, delCallback) {
+  const card = cardTemplate.cloneNode(true);
+  card.querySelector('.card__title').textContent = name;
+  card.querySelector('.card__image').src = imgLink;
+  card.querySelector('.card__delete-button').addEventListener('click', delCallback);
+  return card;
+}
 
-// @todo: Функция удаления карточки
+// Функция удаления карточки
+function deleteCard(event) {
+  const deletedCard = event.target.closest('.card');
+  deletedCard.remove();
+}
 
-// @todo: Вывести карточки на страницу
+// Вывести карточки на страницу
+initialCards.forEach(cardInfo => {
+  const card = createCard(cardInfo.name, cardInfo.link, deleteCard);
+  placesList.append(card)
+});
